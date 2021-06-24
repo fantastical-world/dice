80bf8ade1794c6365ae3d8202450c651d25cb62a
Isaac Gomez <eyeszack@me.com>
2021-06-23 22:09:16 -0700

    exported Dice in Set

--- set.go
+++ set.go
@@ -9,8 +9,8 @@ import (
 //Set holds custom dice that are backed by an expression.
 //You can add dice to your set and roll them as often as needed.
 type Set struct {
-	Name string
-	dice map[string]string
+	Name string            `json:"name"`
+	Dice map[string]string `json:"dice"`
 }
 
 //AddDice will store a roll expression as a custom dice in your set. The name provided can be passed to the RollDice function to roll the expression.
@@ -19,22 +19,22 @@ func (s *Set) AddDice(name string, expression string) error {
 		return fmt.Errorf("error %s is not a valid roll expression", expression)
 	}
 
-	if s.dice == nil {
-		s.dice = make(map[string]string)
+	if s.Dice == nil {
+		s.Dice = make(map[string]string)
 	}
 
-	s.dice[name] = expression
+	s.Dice[name] = expression
 
 	return nil
 }
 
 //RollDice rolls the named custom dice's expression and returns its results.
 func (s *Set) RollDice(name string) (rolls []int, sum int, err error) {
-	if s.dice == nil || len(s.dice) == 0 {
+	if s.Dice == nil || len(s.Dice) == 0 {
 		return rolls, sum, fmt.Errorf("error you do not have any dice in your set")
 	}
 
-	expression := s.dice[name]
+	expression := s.Dice[name]
 
 	if expression == "" {
 		return rolls, sum, fmt.Errorf("error you do not have any dice named [%s] in your set", name)
@@ -47,19 +47,19 @@ func (s *Set) RollDice(name string) (rolls []int, sum int, err error) {
 
 //ListDice returns a string listing of all dice names and expressions in the set.
 func (s *Set) ListDice() string {
-	if s.dice == nil || len(s.dice) == 0 {
+	if s.Dice == nil || len(s.Dice) == 0 {
 		return "no dice"
 	}
 
-	keys := make([]string, 0, len(s.dice))
-	for key := range s.dice {
+	keys := make([]string, 0, len(s.Dice))
+	for key := range s.Dice {
 		keys = append(keys, key)
 	}
 	sort.Strings(keys)
 
 	sb := strings.Builder{}
 	for _, k := range keys {
-		sb.WriteString(fmt.Sprintf("%s %s\n", k, s.dice[k]))
+		sb.WriteString(fmt.Sprintf("%s %s\n", k, s.Dice[k]))
 	}
 	return sb.String()
 }
